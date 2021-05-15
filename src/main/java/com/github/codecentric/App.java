0e26b004b3255dee6edb9961d1eaf086b0147e06
Raffael Stein <raffael.stein@codecentric.de>
2021-05-15 11:26:04 +0200

    Initial project setup

---
+++ src/main/java/com/github/codecentric/App.java
@@ -0,0 +1,9 @@
+package com.github.codecentric;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/codecentric/AppTest.java
@@ -0,0 +1,10 @@
+package com.github.codecentric;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void testApp() {
+    }
+}

9795aea362c9bfe534f012f4ada8f6b327a4b600
Frank Rosner <frank.rosner@datastax.com>
2021-05-21 10:14:30 +0200

    add variables for db user

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,1 +1,13 @@
-variable "astra_client_token" {}
+variable "astra_client_token" {
+  description = "Admin client token for Astra. Needs to have permissions to create databases and keyspaces."
+}
+
+variable "astra_db_client_id" {
+  description = "Database user client ID for Astra. Needs to have permissions to R/W on keyspaces."
+}
+variable "astra_db_client_secret" {
+  description = "Database user client secret for Astra. Needs to have permissions to R/W on keyspaces."
+}
+variable "astra_db_client_token" {
+  description = "Database user client token for Astra. Needs to have permissions to R/W on keyspaces."
+}

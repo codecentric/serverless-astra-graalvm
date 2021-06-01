c71d779f20d5e74803664fa70e02c39136708fcc
Raffael Stein <raffael.stein@codecentric.de>
2021-06-01 08:16:10 +0200

    Revert "Add Cassandra testcontainer"
    This reverts commit c2becc1fc1d6b496df3a201ee5ff04ceded92aa6.

--- src/test/java/com/github/codecentric/ApiTest.java
+++ src/test/java/com/github/codecentric/ApiTest.java
@@ -1,51 +1,32 @@
 package com.github.codecentric;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 public class ApiTest {
-  private final Network cassandraNetwork = Network.newNetwork();
-
-  @Container
-  @SuppressWarnings("rawtypes")
-  public GenericContainer cassandra =
-      new GenericContainer("cassandra:4.0")
-          .withEnv("CASSANDRA_CLUSTER_NAME", "stargate")
-          .withNetwork(cassandraNetwork)
-          .withNetworkAliases("cassandra");
 
   @Container
   @SuppressWarnings("rawtypes")
   public GenericContainer stargate =
       new GenericContainer(DockerImageName.parse("stargateio/stargate-4_0:v1.0.25"))
-          .dependsOn(cassandra)
           .withExposedPorts(8081, 8082)
           .withEnv("CLUSTER_NAME", "stargate")
           .withEnv("CLUSTER_VERSION", "4.0")
-          .withEnv("SEED", "cassandra")
+          .withEnv("DEVELOPER_MODE", "true")
           .withEnv("SIMPLE_SNITCH", "true")
-          .withEnv("ENABLE_AUTH", "true")
-          .withNetwork(cassandraNetwork)
-          .withNetworkAliases("stargate");
+          .withEnv("ENABLE_AUTH", "true");
 
   @Test
-  @Disabled(
-      "Stargate fails to start with error: CassandraRoleManager.java:369 - Setup task failed with error, rescheduling")
   public void testCassandra() throws IOException, InterruptedException {
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request =
@@ -56,6 +37,6 @@ public class ApiTest {
             .header("accept", "application/json")
             .build();
     HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-    assertThat(response.body(), is("It's alive"));
+    System.out.println(response.body());
   }
 }

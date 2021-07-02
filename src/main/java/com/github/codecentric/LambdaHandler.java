9daa89291f471eecd4674e340d023cf1b24accb8
Raffael Stein <raffael.stein@codecentric.de>
2021-07-02 13:23:31 +0200

    Use Map in RequestHandler

--- src/main/java/com/github/codecentric/LambdaHandler.java
+++ src/main/java/com/github/codecentric/LambdaHandler.java
@@ -5,23 +5,32 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Map;
 
-public class LambdaHandler implements RequestHandler<LambdaRequest, LambdaResponse> {
+public class LambdaHandler implements RequestHandler<Map<String, String>, LambdaResponse> {
 
   private static final ObjectMapper mapper = new ObjectMapper();
 
   @Override
-  public LambdaResponse handleRequest(LambdaRequest input, Context context) {
+  public LambdaResponse handleRequest(Map<String, String> input, Context context) {
     String astraUrl = System.getenv("ASTRA_URL");
     String astraToken = System.getenv("ASTRA_TOKEN");
     String astraNamespace = System.getenv("ASTRA_NAMESPACE");
     CassandraClient client = new CassandraClient(URI.create(astraUrl), astraToken, astraNamespace);
+    Order order = new Order();
+
     try {
-      client.saveOrder(input.getOrder());
+      order.setProductName(input.get("product_name"));
+      order.setProductPrice(Integer.parseInt(input.get("product_price")));
+      order.setProductQuantity(Integer.parseInt(input.get("product_quantity")));
+
+      client.saveOrder(order);
     } catch (IOException e) {
       throw new RuntimeException(
-          "Could not save order '"
-              + input.getOrder()
+          "Could not save input '"
+              + input
+              + "' as order '"
+              + order
               + "' at "
               + astraUrl
               + " with namespace "

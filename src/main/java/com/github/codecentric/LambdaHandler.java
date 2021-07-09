aa3ce4520334cfec27a6e7a23650ac0709b8fa5c
Raffael Stein <raffael.stein@codecentric.de>
2021-07-09 09:22:36 +0200

    Use ApiGatewayEvent in LambdaHandler

--- src/main/java/com/github/codecentric/LambdaHandler.java
+++ src/main/java/com/github/codecentric/LambdaHandler.java
@@ -2,17 +2,22 @@ package com.github.codecentric;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Map;
 
-public class LambdaHandler implements RequestHandler<Map<String, String>, LambdaResponse> {
+public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, LambdaResponse> {
 
   private static final ObjectMapper mapper = new ObjectMapper();
 
+  public LambdaHandler() {
+    mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+  }
+
   @Override
-  public LambdaResponse handleRequest(Map<String, String> input, Context context) {
+  public LambdaResponse handleRequest(APIGatewayV2HTTPEvent input, Context context) {
     String astraUrl = System.getenv("ASTRA_URL");
     String astraToken = System.getenv("ASTRA_TOKEN");
     String astraNamespace = System.getenv("ASTRA_NAMESPACE");
@@ -20,11 +25,9 @@ public class LambdaHandler implements RequestHandler<Map<String, String>, Lambda
     Order order = new Order();
 
     try {
-      order.setProductName(input.get("product_name"));
-      order.setProductPrice(Integer.parseInt(input.get("product_price")));
-      order.setProductQuantity(Integer.parseInt(input.get("product_quantity")));
+      LambdaRequest request = mapper.readValue(input.getBody(), LambdaRequest.class);
 
-      client.saveOrder(order);
+      client.saveOrder(request.getOrder());
     } catch (IOException e) {
       throw new RuntimeException(
           "Could not save input '"

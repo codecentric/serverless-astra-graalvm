b7625e4874c79c08fa718726541369fc3a7df207
Raffael Stein <raffael.stein@codecentric.de>
2023-06-25 21:50:49 +0200

    refactor api gw output to api gw terraform file

--- tf/apigateway.tf
+++ tf/apigateway.tf
@@ -1,30 +1,35 @@
 resource "aws_apigatewayv2_api" "api-gateway" {
-  name = "serverless-graal-http-api"
+  name          = "serverless-graal-http-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_stage" "dev-stage" {
-  api_id = aws_apigatewayv2_api.api-gateway.id
-  name = "$default"
+  api_id      = aws_apigatewayv2_api.api-gateway.id
+  name        = "$default"
   auto_deploy = true
 }
 
 resource "aws_apigatewayv2_integration" "api-gateway" {
-  api_id = aws_apigatewayv2_api.api-gateway.id
-  integration_uri = module.lambda_function.lambda_function_invoke_arn
-  integration_type = "AWS_PROXY"
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.api-gateway.id
+  integration_uri        = module.lambda_function.lambda_function_invoke_arn
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "api-gateway-get" {
-  api_id = aws_apigatewayv2_api.api-gateway.id
+  api_id    = aws_apigatewayv2_api.api-gateway.id
   route_key = "GET /order/{orderId}"
-  target = "integrations/${aws_apigatewayv2_integration.api-gateway.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.api-gateway.id}"
 }
 
 resource "aws_apigatewayv2_route" "api-gateway-post" {
-  api_id = aws_apigatewayv2_api.api-gateway.id
+  api_id    = aws_apigatewayv2_api.api-gateway.id
   route_key = "POST /order"
-  target = "integrations/${aws_apigatewayv2_integration.api-gateway.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.api-gateway.id}"
+}
+
+output "aws_api_gateway_endpoint_url" {
+  description = "URL of the AWS API gateway endpoint"
+  value       = aws_apigatewayv2_api.api-gateway.api_endpoint
 }
--- tf/output.tf
+++
@@ -1,4 +0,0 @@
-output "aws_api_gateway_endpoint_url" {
-  description = "URL of the AWS API gateway endpoint"
-  value       = aws_apigatewayv2_api.api-gateway.api_endpoint
-}
